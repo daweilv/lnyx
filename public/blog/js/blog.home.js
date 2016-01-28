1dc00b059215454bc82f5461d4ff65c93bc1bcad
lvdw <code4funlnyx@gmail.com>
2016-01-28 20:35:25 +0800

    home页blog button交互效果

---
+++ public/blog/js/blog.home.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lvdw on 16/1/28.
+ */
+if(typeof blog == 'undefined' || !blog) blog = {};
+
+blog.home = {
+    initEvents: function () {
+        console.log('Blog.home.init');
+        $("#blog_btn").addClass('animated tada');
+    }
+};
+
+$(function () {
+    blog.home.initEvents();
+});
