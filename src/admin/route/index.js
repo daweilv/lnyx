27be0e59033a9d7066d44f442920d15fcd3712a2
lnyx <code4funlnyx@gmail.com>
2016-04-18 14:32:57 +0800

    change router sequence

--- src/admin/route/index.js
+++ src/admin/route/index.js
@@ -7,10 +7,10 @@ var userLogic = require('../logic/user');
 var loginLogic = require('../logic/login');
 
 router.get('/', auth.isLogin, adminLogic.goIndex);
+router.get('/index', auth.isLogin, adminLogic.goIndex);
 router.get('/login', loginLogic.goLogin);
 router.post('/login', loginLogic.login);
 router.get('/logout', loginLogic.logout);
-router.get('/index', auth.isLogin, adminLogic.goIndex);
 router.get('/users', auth.isLogin, userLogic.getUsers);
 router.get('/user/:id', auth.isLogin, userLogic.getUser);
 router.post('/user/', userLogic.saveUser);
