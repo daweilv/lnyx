40f3fe3852e0f2b282be9aa7dbd26fef97d0c572
lnyx <code4funlnyx@gmail.com>
2016-04-20 13:33:07 +0800

    remove comment when pool connected

--- src/common/db/DBInstance.js
+++ src/common/db/DBInstance.js
@@ -6,7 +6,7 @@ var config = require('../config');
 
 var pool = mysql.createPool(config.db);
 pool.on('connection', function (connection) {
-    console.log('pool.on==>connection');
+    //console.log('pool.on==>connection');
 });
 pool.on('enqueue', function () {
     console.log('pool.on==>enqueue. Waiting for available connection slot');
