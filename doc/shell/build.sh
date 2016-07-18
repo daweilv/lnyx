d7bf204ab3b7af7f0c47269f864c9758329a6c0d
lnyx <code4funlnyx@gmail.com>
2016-07-19 00:23:17 +0800

    打包脚本修改,首页修改,去掉无用的npm包

--- doc/shell/build.sh
+++ doc/shell/build.sh
@@ -1,9 +1,6 @@
 cd ~/Projects/lnyx
-rm -rf tmp
-mkdir tmp
-cd tmp
-rm -rf *
-cp -R ../bin ../doc ../node_modules ../src ../test ../tool ../view ../www ../app.js ../package.json .
-tar zcvf deploy.tar.gz *
-scp -P 9103 deploy.tar.gz root@dawei.lv:/home/service/lnyx
-ssh -p 9103 root@dawei.lv
+mkdir -p release
+timestamp=`date +%Y%m%d_%H%M%S`
+tar zcvf ./release/release_${timestamp}.tar.gz bin src view www ./app.js ./package.json
+scp -P 9103 ./release/release_${timestamp}.tar.gz root@jshey.com:/home/download
+ssh -p 9103 root@jshey.com
