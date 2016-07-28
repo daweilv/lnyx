d7ec47667b8b15023e7ac124783d7cc67139f0e6
lnyx <code4funlnyx@gmail.com>
2016-07-28 22:59:54 +0800

    添加重启脚本

--- doc/shell/restart.sh
+++ doc/shell/restart.sh
@@ -1,2 +1,3 @@
-cd /home/service/lnyx
-nohup node ./bin/www > `date +%Y%m%d_%H%M%S`.log &
+cd /home/service
+pkill -f ./lnyx/bin/www
+nohup PORT=80 NODE_ENV=production node ./bin/www > `date +%Y%m%d_%H%M%S`.log &
