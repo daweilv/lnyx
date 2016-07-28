1c751bab47dd84be271ee2628b984b1590e6113b
lnyx <code4funlnyx@gmail.com>
2016-07-28 23:43:49 +0800

    修复重启脚本启动文件错误 打包脚本增加doc文件夹 后台文章分类维护界面放开seq字段

--- doc/shell/build.sh
+++ doc/shell/build.sh
@@ -1,6 +1,6 @@
 cd ~/Projects/lnyx
 mkdir -p release
 timestamp=`date +%Y%m%d_%H%M%S`
-tar zcvf ./release/release_${timestamp}.tar.gz bin src view www ./app.js ./package.json
+tar zcvf ./release/release_${timestamp}.tar.gz bin doc src view www ./app.js ./package.json
 scp -P 9103 ./release/release_${timestamp}.tar.gz root@jshey.com:/home/download
 ssh -p 9103 root@jshey.com
--- doc/shell/restart.sh
+++ doc/shell/restart.sh
@@ -1,3 +1,3 @@
 cd /home/service
 pkill -f ./lnyx/bin/www
-nohup PORT=80 NODE_ENV=production node ./bin/www > `date +%Y%m%d_%H%M%S`.log &
+PORT=80 NODE_ENV=production nohup node ./lnyx/bin/www > `date +%Y%m%d_%H%M%S`.log &
