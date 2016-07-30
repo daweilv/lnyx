a4acdf9323a9bd2266432e7a018b6c3c4091085c
lnyx <code4funlnyx@gmail.com>
2016-07-30 22:03:32 +0800

    表结构优化,分类增加seo相关扩展字段

--- doc/database/f_article_category.sql
+++ doc/database/f_article_category.sql
@@ -3,6 +3,10 @@ CREATE TABLE `f_article_category` (
   `parent_id` varchar(30) NOT NULL DEFAULT '0' COMMENT '父id',
   `name` varchar(50) DEFAULT NULL COMMENT '名称',
   `seq` int(11) DEFAULT NULL COMMENT '排序号',
+  `is_show` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否可见{0:不可见,1:可见}',
+  `seo_url` varchar(50) DEFAULT NULL COMMENT 'SEO链接',
+  `seo_keywords` varchar(200) DEFAULT NULL COMMENT 'SEO关键词',
+  `seo_description` varchar(200) DEFAULT NULL COMMENT 'SEO描述',
   `create_at` varchar(24) DEFAULT NULL COMMENT '创建日期',
   `create_by` varchar(30) DEFAULT NULL COMMENT '创建人',
   `update_at` varchar(24) DEFAULT NULL COMMENT '更新日期',
@@ -10,5 +14,5 @@ CREATE TABLE `f_article_category` (
   `delete_at` varchar(24) DEFAULT NULL COMMENT '删除日期',
   `delete_by` varchar(30) DEFAULT NULL COMMENT '删除人',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `seo_url` (`seo_url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章分类';
--- doc/database/f_visit.sql
+++ doc/database/f_visit.sql
@@ -1,12 +1,13 @@
 CREATE TABLE `f_visit` (
   `id` varchar(30) NOT NULL COMMENT 'id',
-  `ip` varchar(30) NOT NULL COMMENT 'ip地址',
-  `geolocation` varchar(100) NOT NULL COMMENT '地理位置',
-  `useragent` varchar(500) NOT NULL COMMENT '用户代理',
-  `browser` varchar(10) NOT NULL COMMENT '浏览器',
-  `url` varchar(200) NOT NULL COMMENT '访问地址',
-  `param` varchar(200) NOT NULL COMMENT '查询参数',
   `session_id` varchar(50) NOT NULL COMMENT '会话id',
-  `view_at` varchar(24) DEFAULT NULL COMMENT '浏览日期',
+  `ip` varchar(30) NOT NULL COMMENT 'ip地址',
+  `geolocation` varchar(100) DEFAULT NULL COMMENT '地理位置',
+  `useragent` varchar(500) DEFAULT NULL COMMENT '用户代理',
+  `browser` varchar(10) DEFAULT NULL COMMENT '浏览器',
+  `referrer` varchar(500) DEFAULT NULL COMMENT '来源',
+  `url` varchar(500) NOT NULL DEFAULT '' COMMENT '访问地址',
+  `param` varchar(200) DEFAULT NULL COMMENT '查询参数',
+  `view_at` varchar(24) NOT NULL COMMENT '浏览日期',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='访问记录';
